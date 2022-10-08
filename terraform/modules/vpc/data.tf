984dcb0a7caebb986ed8898fffefe3f6aab34e50
chandralekhasingasani <chandralekha.s@nutanix.com>
2022-10-08 18:23:33 +0530

    Learning

--- terraform/modules/vpc/data.tf
+++ terraform/modules/vpc/data.tf
@@ -5,5 +5,5 @@ data "aws_availability_zones" "available" {
 }
 
 output "availability_count" {
-  value = length(data.aws_availability_zones.available.*.names)
+  value = length(data.aws_availability_zones.available.names)
 }
