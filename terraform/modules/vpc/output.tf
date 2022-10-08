abcf3bfc66788787d33f5321ab304d9f10d1b560
chandralekhasingasani <chandralekha.s@nutanix.com>
2022-10-08 18:40:11 +0530

    Learning

---
+++ terraform/modules/vpc/output.tf
@@ -0,0 +1,11 @@
+output "VPC_ID" {
+  value = aws_vpc.main.id
+}
+
+output "PRIVATE_SUBNET_IDS" {
+  value = aws_subnet.private-subnet.*.id
+}
+
+output "PUBLIC_SUBNET_IDS" {
+  value = aws_subnet.private-subnet.*.id
+}
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,7 +1,7 @@
 # Configure the AWS Provider
-provider "aws" {
+/*provider "aws" {
   region = "us-east-1"
-}
+}*/
 
 module "vpc" {
   source = "./modules/vpc"
