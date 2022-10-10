326354530d353289391ba3cdc7041747e1389b00
chandralekhasingasani <chandralekha.s@nutanix.com>
2022-10-10 21:39:26 +0530

    Learning

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -38,7 +38,7 @@ resource "aws_s3_bucket_object" "object" {
 resource "aws_instance" "web" {
   depends_on    = [aws_key_pair.deployer]
   count         = 1
-  ami           = "ami-0bb6af715826253bf"
+  ami           = data.aws_ami.centos.id
   instance_type = "t3.micro"
   subnet_id     = element(var.PUBLIC_SUBNET_IDS,count.index)
   key_name      = aws_key_pair.deployer.key_name
