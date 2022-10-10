7ceaca44dd4052911a3935e4bac809d802c00d3d
chandralekhasingasani <chandralekha.s@nutanix.com>
2022-10-10 14:47:55 +0530

    Learning

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -1,7 +1,35 @@
-data "aws_ami" "ami" {
-  name_regex = "Centos-7-DevOps-Practice"
+resource "null_resource" "pem_creation" {
+  provisioner "local-exec" {
+    command = "yes y |ssh-keygen -q -t rsa -f deployer -N ''"
+  }
 }
 
-output "ami_id" {
-  value = data.aws_ami.ami.id
+module "pub_content"{
+  source  = "matti/resource/shell"
+  command = "cat deployer.pub"
+}
+
+
+module "pem_content"{
+  source  = "matti/resource/shell"
+  command = "cat deployer"
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = module.pub_content.stdout
+}
+
+resource "aws_s3_bucket" "terraform-batch38" {
+  bucket = "terraform-batch38"
+  tags = {
+    Name        = "terraform-batch38"
+  }
+}
+
+# Upload an object
+resource "aws_s3_bucket_object" "object" {
+  bucket = terraform-batch38
+  key    = "deployer"
+  source = "deployer"
 }
