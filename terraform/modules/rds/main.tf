ca5e102f3169a9e1c8d1f39c5914c88df19680f6
chandralekhasingasani <chandralekha.s@nutanix.com>
2022-10-10 13:12:59 +0530

    Learning

--- terraform/modules/rds/main.tf
+++ terraform/modules/rds/main.tf
@@ -7,7 +7,7 @@ resource "aws_db_instance" "default" {
     password             = var.DB_PASSWORD
     parameter_group_name = aws_db_parameter_group.mariadb.id
     skip_final_snapshot  = true
-    vpc_security_group_ids = aws_security_group.allow_db_traffic.id
+    vpc_security_group_ids = [aws_security_group.allow_db_traffic.id]
     storage_type = "gp2"
     db_subnet_group_name = aws_db_subnet_group.mariadb-subnet-group.id
 }
