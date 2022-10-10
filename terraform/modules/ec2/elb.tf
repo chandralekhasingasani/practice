1ec6a7594966c336638c47dd6f18cfefab18f46d
chandralekhasingasani <chandralekha.s@nutanix.com>
2022-10-10 15:26:50 +0530

    Learning

--- terraform/modules/ec2/elb.tf
+++ terraform/modules/ec2/elb.tf
@@ -15,7 +15,7 @@ resource "aws_elb" "bar" {
     healthy_threshold   = 2
     unhealthy_threshold = 2
     timeout             = 3
-    target              = "HTTP:80"
+    target              = "HTTP:80/"
     interval            = 30
   }
 
